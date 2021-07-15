280db48862c431417f3e29ff59c370a2a24d0bed
Ali Behbahani <dr.ali.behbahani18@gmail.com>
2021-07-15 19:57:41 +0300

    skip anywhere

--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,26 @@
 //react imports
 import React from "react";
+import { View, TouchableWithoutFeedback } from "react-native";
+
 //styles
 import { Title, BGImage, HomeView, TextStyled } from "../styles";
 const Home = ({ navigation }) => {
+  const next = () => {
+    navigation.navigate("GalleryList");
+  };
   return (
-    <BGImage source={{ uri: "https://wallpaperaccess.com/full/42337.jpg" }}>
-      <HomeView>
-        <Title>The Nifty Shop</Title>
-        <TextStyled onPress={() => navigation.navigate("GalleryList")}>
-          Skip
-        </TextStyled>
-      </HomeView>
-    </BGImage>
+    <View style={{ flex: 1, backgroundColor: "rgba(52, 52, 52, alpha)" }}>
+      <TouchableWithoutFeedback onPress={next}>
+        <BGImage source={{ uri: "https://wallpaperaccess.com/full/42337.jpg" }}>
+          <HomeView>
+            <Title onPress={() => navigation.navigate("GalleryList")}>
+              The Nifty Shop
+            </Title>
+            <TextStyled>click anywhere to skip!</TextStyled>
+          </HomeView>
+        </BGImage>
+      </TouchableWithoutFeedback>
+    </View>
   );
 };
 
